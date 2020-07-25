fcc32fc0ae604c0a77db9f560982777dde899fcb
amaladwa <ali_maladwala@sfu.ca>
2020-07-24 18:38:50 -0700

    Add score column to user model

--- project/db/schema.rb
+++ project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_23_023133) do
+ActiveRecord::Schema.define(version: 2020_07_25_013015) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_07_23_023133) do
     t.boolean "is_admin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "score"
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
