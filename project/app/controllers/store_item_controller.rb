88ec9981b01d8ba6b6d3d271f7e46e86c6007b49
amaladwa <ali_maladwala@sfu.ca>
2020-07-24 23:31:54 -0700

    Enforce login requirement for store

--- project/app/controllers/store_item_controller.rb
+++ project/app/controllers/store_item_controller.rb
@@ -2,34 +2,31 @@ class StoreItemController < ApplicationController
 
 	def buy
 	
-
 		user = User.find_by_id(session[:user_id])
 		logger.debug user.inspect
 		logger.debug user.store_items.inspect	
 		logger.debug "Number of store items bought: "
 		logger.debug user.store_items.size 
 		
-		@store_item = user.store_items.create(name: "testStoreItem")
 		
-		if user.store_items.any?{|item| item.name = @store_item.name}
+		if user.store_items.any?{|item| item.name = "testStoreItem"}
 			flash.now[:alert] = "You already own that item!"
 			logger.debug "DUPLICATE ITEM"			
-			return
+			return			
 		end
 		
 		
-		logger.debug "NEW ITEM BOUGHT"	
-		
+	
+		@store_item = user.store_items.create(name: "testStoreItem")		
 		respond_to do |format|
 		  if @store_item.save
+		  	logger.debug "NEW ITEM BOUGHT"
 			format.html { redirect_to store_url, notice: 'Item was successfully bought and added to the current user' }
 		  else
 			format.html { render :new }
 		  end
 		end		
 		 
-	 
 	end
-
 
 end
