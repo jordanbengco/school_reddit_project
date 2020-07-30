588e26ce3836590f28ad7b7bcd54dffc9a286f90
Jordon123 <46872554+Jordon123@users.noreply.github.com>
2020-07-29 23:05:51 -0700

    increment score

--- project/app/controllers/likes_controller.rb
+++ project/app/controllers/likes_controller.rb
@@ -1,11 +1,29 @@
 class LikesController < ApplicationController
   before_action :find_article
   def create
-    @like = @article.likes.create(user_id: current_user.id, article_id: @article.id)
-    redirect_to article_path(@article)
+    if !(already_liked?)
+      #like
+      @like = @article.likes.create(user_id: current_user.id, article_id: @article.id)
+      @originalUser = @article.author
+      @originalUserId = User.find_by_username(@originalUser)
+      @originalUserId.increment!(:score)
+      redirect_to article_path(@article)
+    elsif already_liked?
+      #unlike
+      @like = Like.find_by(params[user_id: current_user.id, article_id: @article.id])
+      @originalUser = @article.author
+      @originalUserId = User.find_by_username(@originalUser)
+      @originalUserId.decrement!(:score)
+      @like.destroy
+      redirect_to article_path(@article)
+    end
   end
-  
+
   private
+
+  def already_liked?
+    Like.where(user_id: current_user.id, article_id: @article.id).exists?
+  end
 
   def find_article
     @article = Article.find(params[:article_id])
