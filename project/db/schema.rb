22181e333a4e1f3a64f6b71b1ff67e204dd5af20
Jordan Bengco <jordan_bengco@sfu.ca>
2020-07-29 20:17:57 -0700

    Ran db:migrate to update schema

--- project/db/schema.rb
+++ project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_220611) do
+ActiveRecord::Schema.define(version: 2020_07_29_063059) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_07_28_220611) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "author"
     t.datetime "time"
+    t.text "edit"
   end
 
   create_table "comments", force: :cascade do |t|
