29b8ad9ffc9c8985deeba1352af8c41127acfa4c
amaladwa <ali_maladwala@sfu.ca>
2020-08-07 23:48:17 -0700

    Implement youtube tag and first pass at emoji tag logic

--- project/app/controllers/articles_controller.rb
+++ project/app/controllers/articles_controller.rb
@@ -1,4 +1,31 @@
 class ArticlesController < ApplicationController
+  helper_method :handle_custom_tags
+  def handle_custom_tags(text)
+	text = ActionController::Base.helpers.sanitize(text)
+
+	#find and replace all emoji tags with the proper image. TODO: externalize this logic to read from two DB columns in the store_item table
+	#check that the author has access to each emoji
+	if current_user.is_admin || current_user.store_items.any?{|item| item.name == 'Hello World Emoticon'}
+		text = text.gsub("[helloWorld]",'<img src="/emoticons/HelloWorld.jpg" width="128px" height="128px"/>');
+	end
+	#find and replace all youtube tags with the proper iframe code
+	#[youtube: dQw4w9WgXcQ] input example
+	matches = text.scan(/\[youtube:\s\S*\]/)
+	youtubeVideoIDs = matches.clone
+	youtubeVideoIDs.map! {|id|
+		id = id[9..-2].strip
+		id = ActionController::Base.helpers.sanitize(id)
+	}
+	matches.each_with_index do |ytLink, index|
+		youtubeIframe = '<iframe width="640" height="480" src="http://www.youtube.com/embed/' + youtubeVideoIDs[index] + '" frameborder="0"></iframe>'
+		text = text.gsub(ytLink,youtubeIframe);
+	end
+
+	#do the same for video and audio html tags
+
+
+	return text
+  end
 
   def index
     @articles = Article.all
@@ -18,7 +45,9 @@ class ArticlesController < ApplicationController
  
   def create
     @article = Article.new(article_params)
-	@article.text = ActionController::Base.helpers.sanitize(@article.text)
+
+	#TODO: change to write to a seperate render string, so the user can continue to edit in their original format
+	@article.text = handle_custom_tags(@article.text)
 
     if @article.save
       redirect_to @article
@@ -30,6 +59,9 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
 	params[:article][:text] = ActionController::Base.helpers.sanitize(params[:article][:text])
+
+	#TODO: change to write to a seperate render string, so the user can continue to edit in their original format
+	params[:article][:text] = handle_custom_tags(params[:article][:text])
 
     if @article.update(article_params)
       redirect_to @article
--- project/db/seeds.rb
+++ project/db/seeds.rb
@@ -17,4 +17,9 @@ StoreItemListing.create(name: 'Test item 1', description: 'This is test item 1',
 StoreItemListing.create(name: 'Test item 2', description: 'This is test item 2', cost: 2)
 StoreItemListing.create(name: 'Test item 3', description: 'This is test item 3', cost: 3)
 
+StoreItemListing.create(name: 'Hello World Emoticon', description: 'Allows you to type [helloWorld] in your posts to show a custom emoji.', cost: 1)
+
+
 Article.create(title: 'Hello World!', text: '<b>Hello World, here is an image:</b><br><img src="https://live.staticflickr.com/79/234433736_1b5e1f02cd_z.jpg" width="20%" height="20%"/><br>', author: 'admin')
+Article.create(title: 'Custom Emoticon Test', text: '<b>Custom Emoticon:<br> [helloWorld] <br>', author: 'user1')
+Article.create(title: 'Youtube Test', text: '<b>Youtube Video:<br> [youtube: dQw4w9WgXcQ] <br>', author: 'user2')
