b2facc2eea5328ed404dd215e19f57ad1c7212fb
Rahul Anand <raa37@sfu.ca>
2020-08-08 18:09:22 -0700

    Added production config for email notification

--- project/config/environments/development.rb
+++ project/config/environments/development.rb
@@ -61,13 +61,15 @@ Rails.application.configure do
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.delivery_method = :smtp
-# SMTP settings for gmail
-config.action_mailer.smtp_settings = {
- :address              => "smtp.gmail.com",
- :port                 => 587,
- :user_name            => ENV['gmail_username'],
- :password             => ENV['gmail_password'],
- :authentication       => "plain",
-:enable_starttls_auto => true
-}
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+        :address              => "smtp.gmail.com",
+        :port                 => 587,
+        :user_name            => ENV['gmail_username'],
+        :password             => ENV['gmail_password'],
+        :authentication       => "plain",
+        :enable_starttls_auto => true
+  }
+  # Note: if emails to slow, switch from gmail to mandrill, railgun, etc.
+
 end
