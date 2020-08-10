635470ee2bb3c94cb8a48d91504702eb1b0f1d9e
Jordan Bengco <jordan_bengco@sfu.ca>
2020-08-10 15:57:40 -0700

    Fix bracket error and added more liked posts in seed file

--- project/db/seeds.rb
+++ project/db/seeds.rb
@@ -6,15 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: 'admin', email: 'admin@fakeemail.com', password: 'admin', password_confirmation: 'admin',is_admin: true, score: 100, about: 'dont test me')
+User.create(username: 'admin', email: 'admin@fakeemail.com', password: 'admin', password_confirmation: 'admin',is_admin: true, score: 100, about: 'Did you ever hear the tragedy of Darth Plagueis The Wise? 
+	I thought not. Its not a story the Jedi would tell you. Its a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians 
+	to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be 
+	unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his 
+	apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.')
 User.create(username: 'admin2', email: 'admin2@fakeemail.com', password: 'admin2', password_confirmation: 'admin2',is_admin: true, score: 100, about: 'I let this small amount of power get to my head')
 
 User.create(username: 'joe', email: 'joe@fakeemail.com', password: 'joe', password_confirmation: 'joe',is_admin: false, score: 10)
 User.create(username: 'bob', email: 'bob@fakeemail.com', password: 'bob', password_confirmation: 'bob',is_admin: false, score: 6)
 User.create(username: 'jordan', email: 'jordan@fakeemail.com', password: 'jordan', password_confirmation: 'jordan',is_admin: false, score: 2, about: 'cmpt 470 student working on this project!')
 User.create(username: 'ali', email: 'ali@fakeemail.com', password: 'ali', password_confirmation: 'ali',is_admin: false, score: 2, about: 'cmpt 470 student working on this project!')
-User.create(username: 'rahul', email: 'rahul@fakeemail.com', password: 'rahul', password_confirmation: 'rahul',is_admin: false, score: 2, about: 'cmpt 470 student working on this project!'))
-User.create(username: 'jordon', email: 'jordon@fakeemail.com', password: 'jordon', password_confirmation: 'jordon',is_admin: false, score: 2, about: 'cmpt 470 student working on this project!'))
+User.create(username: 'rahul', email: 'rahul@fakeemail.com', password: 'rahul', password_confirmation: 'rahul',is_admin: false, score: 2, about: 'cmpt 470 student working on this project!')
+User.create(username: 'jordon', email: 'jordon@fakeemail.com', password: 'jordon', password_confirmation: 'jordon',is_admin: false, score: 2, about: 'cmpt 470 student working on this project!')
 User.create(username: 'amy', email: 'amy@fakeemail.com', password: 'amy', password_confirmation: 'amy',is_admin: false, score: 9)
 User.create(username: 'tim', email: 'tim@fakeemail.com', password: 'tim', password_confirmation: 'tim',is_admin: false, score: 7)
 User.create(username: 'courtney', email: 'courtney@fakeemail.com', password: 'courtney', password_confirmation: 'courtney',is_admin: false, score: 15, about: 'Hello i am a girl btw and not a 40 year old male')
@@ -37,7 +41,7 @@ Category.create(name: 'Discussion') #9
 Article.create(title: 'Hello World!', text: '<b>Hello World, here is an image:</b><br><img src="https://live.staticflickr.com/79/234433736_1b5e1f02cd_z.jpg" width="20%" height="20%"/><br>', author: 'admin', category_id: '1', created_at: '2020-08-09 03:51:23.082128')
 Article.create(title: 'Custom Emoticon Test', text: '<b>Custom Emoticons:<br> [helloWorld] <br>[starEmoji] <br>', author: 'admin', category_id: '1', created_at: '2020-08-09 04:23:45.082128')
 Article.create(title: 'Youtube Test', text: '<b>Youtube Video:<br> [youtube: dQw4w9WgXcQ] <br>', author: 'bob', category_id: '7', created_at: '2020-08-08 16:54:33.322128')
-Article.create(title: 'Go Canucks!', text: 'I cannot believe we qualified for the playoffs! Go Canucks Go!', author: 'jordan', category_id: '3', created_at: '2020-08-09 01:12:32.082128')
+Article.create(title: 'Go Canucks! We play the St. Louis Blues in the first round', text: 'I cannot believe we qualified for the playoffs! Hope we do well!', author: 'jordan', category_id: '3', created_at: '2020-08-09 01:12:32.082128')
 Article.create(title: 'Why did the chicken cross the road?', text: 'To get to the other side', author: 'nicole', category_id: '2', created_at: '2020-08-09 03:53:25.082128')
 Article.create(title: 'How is everyone finding school changes due to coronavirus?', text: 'Easier or harder?', author: 'tim', category_id: '9', created_at: '2020-08-09 02:33:44.082128')
 Article.create(title: 'I think UBC is better than SFU!', text: 'Why the dislikes guys?', author: 'bob', edit: 'bob', category_id: '9', created_at: '2020-08-08 22:33:43.082128')
@@ -47,10 +51,12 @@ Article.create(title: 'Audio and Video Test', text: '[video: https://www.nps.gov
 author: 'user3', category_id: '1')
 
 a_YouTube = Article.find_by_title('Youtube Test')
-a_Canucks = Article.find_by_title('Go Canucks!')
+a_Canucks = Article.find_by_title('Go Canucks! We play the St. Louis Blues in the first round')
 a_HelloWorld = Article.find_by_title('Hello World!')
 a_Chicken = Article.find_by_title('Why did the chicken cross the road?')
 a_UBC = Article.find_by_title('I think UBC is better than SFU!')
+a_School = Article.find_by_title('How is everyone finding school changes due to coronavirus?')
+a_Emote = Article.find_by_title('Custom Emoticon Test')
 
 u_admin = User.find_by_username('admin')
 u_bob = User.find_by_username('bob')
@@ -63,6 +69,9 @@ Like.create(article_id: a_YouTube.id, user_id:u_admin.id)
 Like.create(article_id: a_YouTube.id, user_id:u_tim.id)
 Like.create(article_id: a_YouTube.id, user_id:u_nicole.id)
 Dislike.create(article_id: a_YouTube.id, user_id:u_amy.id)
+
+Like.create(article_id: a_School.id, user_id:u_tim.id)
+Like.create(article_id: a_Emote.id, user_id:u_tim.id)
 
 Like.create(article_id: a_Canucks.id, user_id:u_bob.id)
 Like.create(article_id: a_Canucks.id, user_id:u_tim.id)
