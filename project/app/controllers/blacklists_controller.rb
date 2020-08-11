fee1d06029bc625fdd98b7fbb98f2a5348773d82
Rahul Anand <raa37@sfu.ca>
2020-08-10 19:46:58 -0700

    Blacklisting users, WIP

--- project/app/controllers/blacklists_controller.rb
+++ project/app/controllers/blacklists_controller.rb
@@ -1,59 +1,52 @@
 class BlacklistsController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  def index
+    @blacklists = Blacklist.all
+  end
+ 
+  def show
+    @blacklist = Blacklist.find(params[:id])
+  end
+ 
+  def new
+    @blacklist = Blacklist.new
+  end
+ 
+  def edit
+    @blacklist = Blacklist.find(params[:id])
+  end
+ 
+  def create
+    @user = User.find_by_slug(params[:slug])
+    @blacklist = @user.blacklists.create(blacklist_params)
 
-    def index
-      @blacklists = Blacklist.all
+    if @blacklist.save
+      redirect_to @blacklist
+    else
+      render 'new'
     end
-     
-    def show
-      @blacklist = Blacklist.find(params[:id])
+  end
+ 
+  # def update
+  #   @blacklist = Blacklist.find(params[:id])
+ 
+  #   if @blacklist.update(blacklist_params)
+  #     redirect_to @blacklist
+  #   else
+  #     render 'edit'
+  #   end
+  # end
+ 
+  # def destroy
+  #   @blacklist = Blacklist.find(params[:id])
+  #   @blacklist.destroy
+ 
+  #   redirect_to root_path
+  # end
+ 
+  private
+    def blacklist_params
+      params.require(:blacklist).permit(:blocked)
     end
-     
-    def new
-      @blacklist = Blacklist.new
-    end
-     
-    def edit
-      @blacklist = Blacklist.find(params[:id])
-    end
-     
-    def create
-      @blacklist = Blacklist.new(blacklist_params)
-     
-      if @blacklist.save
-        redirect_to @blacklist
-      else
-        render 'new'
-      end
-    end
-     
-    def update
-      @blacklist = Blacklist.find(params[:id])
-     
-      if @blacklist.update(blacklist_params)
-        redirect_to @blacklist
-      else
-        render 'edit'
-      end
-    end
-     
-    def destroy
-      @blacklist = Blacklist.find(params[:id])
-      @blacklist.destroy
-     
-      redirect_to blacklists_path
-    end
-     
-    private
-      def blacklist_params
-        params.require(:blacklist).permit(:blocked)
-      end
-
-      def set_user
-        @user = User.find_by_slug(params[:slug])
-      end
-  
-    
 
 
 end
--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   resources :categories
-  #resources :blacklists
 
 
   root 'main#index'
